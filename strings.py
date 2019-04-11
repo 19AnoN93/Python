c8cd5fbf7caf25165f892509d08f6b9ef6261d7d
19AnoN93 <arkhipov-ignat@yandex.ru>
2019-04-11 19:39:54 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -2,3 +2,20 @@ a = "__________5___________"
 print(a)
 a = a.rstrip('___')
 print(a)
+
+print(" ")
+
+b = "Мама мыла раму xD"
+print(b)
+b = b.replace('раму', 'папу', 1)
+print(b)
+b = b.replace('мыла', 'била', 1)
+print(b)
+
+print(" ")
+
+c = "мама мыла раму"
+c = c.upper()
+c = c.split()
+for i in c:
+    print(i)
